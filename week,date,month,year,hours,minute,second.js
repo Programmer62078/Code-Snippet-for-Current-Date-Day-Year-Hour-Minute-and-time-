154c2863edc5b2c6653c5eaeb7c6eb34ab62f594
Md Salman Khan <151425006+Programmer62078@users.noreply.github.com>
2024-12-28 23:27:56 +0530

    Update week,date,month,year,hours,minute,second.js

--- week,date,month,year,hours,minute,second.js
+++ week,date,month,year,hours,minute,second.js
@@ -1,17 +1,5 @@
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
 ];
 
 function getDate() {
